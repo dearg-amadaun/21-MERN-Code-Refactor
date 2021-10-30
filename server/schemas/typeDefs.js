a0c2df1677c6e511e515b47046936ce6d597d8a1
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-10-29 23:45:57 -0400

    define typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,35 +1,38 @@
-const { gql } = require('apollo-server-express');
-
+//Reference user-controller.js, book model, and user model.
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Profile {
+  type Query {
+    me: User
+  }
+
+  type User {
     _id: ID
-    name: String
+    username: String
     email: String
-    password: String
-    skills: [String]!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
   }
 
   type Auth {
-    token: ID!
-    profile: Profile
-  }
-
-  type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
-    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
-    me: Profile
+      token: ID!
+      user: User
   }
 
   type Mutation {
-    addProfile(name: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile: Profile
-    removeSkill(skill: String!): Profile
+    saveBook(authors: [String], description: String. title: String, bookId: String, image: String, link: String): User
+    removeBook(BookId: String): User
   }
 `;
 
