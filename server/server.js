e0ad00dfd65527690b828fc39263cc4cd7392b9f
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-11-12 22:05:20 -0500

    still not working

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-async function runApolloServer() {
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -43,6 +43,6 @@ db.once('open', () => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
  });
-}
+
 
 
